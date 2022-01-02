14551fbbcd250071c6ac57e2c9894b7e15d242ef
Shubham Jain <shubhamjain1922@gmail.com>
2022-01-02 13:29:17 +0530

    Create 9.cpp

---
+++ Goldman Sachs/9.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public : 
+    string printMinNumberForPattern(string s){
+        string res="";
+        stack<int> st;
+        int num=1;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='D'){
+                st.push(num++);
+            }
+            else
+            {
+                st.push(num++);
+                while(!st.empty()){
+                    res+=to_string(st.top());
+                    st.pop();
+                }
+            }
+        }
+        st.push(num++);
+        while(!st.empty()){
+            res+=to_string(st.top());
+            st.pop();
+        }
+        
+        return res;
+    }
+}
