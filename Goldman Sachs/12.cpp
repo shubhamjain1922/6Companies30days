479e308fcdc6a7ba55306c1670a844bf296c643c
Shubham Jain <shubhamjain1922@gmail.com>
2022-01-03 15:19:57 +0530

    Create 12.cpp

---
+++ Goldman Sachs/12.cpp
@@ -0,0 +1,11 @@
+Total number of squares in N*N chessboard = n(n+1)(2n+1)/6
+  
+Number of 1*1 square = n*n
+Number of 2*2 square = n-1*n-1
+  .
+  .
+  .
+  .
+Number of n*n square = 1*1
+
+Therefore, total square = 1^2 + 2^2 + .... + n^2 = n(n+1)(2n+1)/6
