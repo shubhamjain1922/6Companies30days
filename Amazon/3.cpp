cb1f5c2a6424096be042d539c50b96bbf8d007c5
Shubham Jain <shubhamjain1922@gmail.com>
2022-01-08 18:20:37 +0530

    Create 3.cpp

---
+++ Amazon/3.cpp
@@ -0,0 +1,22 @@
+class Solution{
+public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        int i=0,j=0,m=0;
+        for(;j<k-1;j++){
+            m=max(m,arr[j]);
+        }
+        vector<int> res;
+        while(j<n){
+            m=max(m,arr[j]);
+            res.push_back(m);
+            if(m==arr[i]){
+                m=0;
+                for(int l=i+1;l<=j;l++)
+                    m=max(m,arr[l]);
+            }
+            i++;
+            j++;
+        }
+        return res;
+    }
+};
