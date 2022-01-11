a104d2f4868400d46ce03b3fbfa5c44ba4f269d7
Shubham Jain <shubhamjain1922@gmail.com>
2022-01-11 11:36:14 +0530

    Create 4.cpp

---
+++ Microsoft/4.cpp
@@ -0,0 +1,41 @@
+class Solution
+{   
+    public: 
+    vector<int> spirallyTraverse(vector<vector<int> > mat, int r, int c) 
+    {
+        int left=0,top=0,bottom=r-1,right=c-1;
+        vector<int> v;
+        int dir=1;
+        while(left<=right && top<=bottom){
+            if(dir==1){
+            for(int i=left;i<=right;i++){
+                v.push_back(mat[top][i]);
+            }
+            top++;
+            dir=2;
+            }
+            else if(dir==2){
+            for(int i=top;i<=bottom;i++){
+                v.push_back(mat[i][right]);
+            }
+            right--;
+            dir=3;
+            }
+            else if(dir==3){
+            for(int i=right;i>=left;i--){
+                v.push_back(mat[bottom][i]);
+            }
+            bottom--;
+            dir=4;
+            }
+            else if(dir==4){
+            for(int i=bottom;i>=top;i--){
+                v.push_back(mat[i][left]);
+            }
+            left++;
+            dir=1;
+            }
+        }
+        return v;
+    }
+};
