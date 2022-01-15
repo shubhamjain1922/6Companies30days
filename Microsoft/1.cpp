c6063ed63236e9b571278b488ae4c7209fa0c809
Shubham Jain <shubhamjain1922@gmail.com>
2022-01-15 15:27:27 +0530

    Create 1.cpp

---
+++ Microsoft/1.cpp
@@ -0,0 +1,31 @@
+class Solution{
+
+  public:
+
+int minDifference(int arr[], int n)  { 
+    int sum=0;
+       for(int i=0;i<n;i++){
+           sum+=arr[i];
+       }
+       int temp=0;
+       if(sum%2!=0) temp=1;
+       sum=sum/2;
+       int dp[n+1][sum+1];
+       for(int i=0;i<n+1;i++){
+           dp[i][0]=0;
+       }
+       for(int i=0;i<sum+1;i++){
+           dp[0][i]=0;
+       }
+       for(int i=1;i<n+1;i++){
+           for(int j=1;j<sum+1;j++){
+               if(arr[i-1]<=j)
+                   dp[i][j]=max(arr[i-1]+dp[i-1][j-arr[i-1]],dp[i-1][j]);
+               else
+                   dp[i][j]=dp[i-1][j];
+           }
+       }
+       int ans =dp[n][sum];
+       return (2*sum+temp-ans)-ans;
+} 
+};
