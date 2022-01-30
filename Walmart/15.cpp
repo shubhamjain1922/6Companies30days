66e1828f1ff508df7e19220af9de0cf48ad0250e
Shubham Jain <shubhamjain1922@gmail.com>
2022-01-30 11:27:01 +0530

    Update 15.cpp

--- Walmart/15.cpp
+++ Walmart/15.cpp
@@ -1,10 +1,21 @@
-int divide(int A, int B) {
-        if (A == INT_MIN && B == -1) return INT_MAX;
-        int a = abs(A), b = abs(B), res = 0, x = 0;
-        while (a - b >= 0) {
-            for (x = 0; a - (b << x << 1) >= 0; x++);
-            res += 1 << x;
-            a -= b << x;
-        }
-        return (A > 0) == (B > 0) ? res : -res;
+class Solution {
+public:
+    int divide(int A, int B) {
+    if (A == INT_MIN && B == -1) return INT_MAX;
+    if (A == INT_MIN && B == 1) return INT_MIN;
+    if (B == INT_MIN) return A == INT_MIN;
+    int a = abs(A), b = abs(B), res = 0, n = 0;
+    if (A == INT_MIN){
+        res = 1;
+        a = abs(A + b);
     }
+    while (a >= b){
+        while ((b<<n) <= INT_MAX/2 && a >= (b<<n)) ++n;
+        if (a < (b<<n)) --n;
+        a -= b<<n;
+        res += 1<<n;
+        n = 0;
+    }
+    return (A >= 0) == (B >= 0)?res:-res;
+}
+};
